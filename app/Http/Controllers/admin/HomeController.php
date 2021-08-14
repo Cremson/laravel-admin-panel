640ef2600afba0d6b01622f59541ea2116e41fea
Cremson <mikemahnet@gmail.com>
2021-08-14 18:08:29 +0300

    default admin panel

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-      return view('admin.home.index');
+        $pots_count = Post::all()->count();
+      return view('admin.home.index', [
+          'pots_count' => $pots_count
+      ]);
     }
 }
