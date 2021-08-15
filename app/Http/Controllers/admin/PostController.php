7aa143280c241b88ba4a6ad0532e951768556a8c
Cremson <mikemahnet@gmail.com>
2021-08-15 19:08:50 +0300

    Displaying all posts and editing them

--- app/Http/Controllers/admin/PostController.php
+++ app/Http/Controllers/admin/PostController.php
@@ -16,7 +16,11 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::orderBy('created_at', 'DESC')->get();
+
+        return view('admin.post.index', [
+            'posts' => $posts
+        ]);
     }
 
     /**
@@ -41,7 +45,14 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $post = new Post();
+        $post->title = $request->title;
+        $post->img = $request->img;
+        $post->text = $request->text;
+        $post->cat_id = $request->cat_id;
+        $post->save();
+        return redirect()->back()->withSuccess('Статья была успешно добавленна');
+
     }
 
     /**
@@ -63,7 +74,12 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        $categories = Category::orderBy('created_at', 'DESC')->get();
+
+        return view('admin.post.edit', [
+            'categories' => $categories,
+            'post' => $post,
+        ]);
     }
 
     /**
@@ -75,7 +91,13 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $post->title = $request->title;
+        $post->img = $request->img;
+        $post->text = $request->text;
+        $post->cat_id = $request->cat_id;
+        $post->save();
+
+        return redirect()->back()->withSuccess('Статья была успешно обновлена!');
     }
 
     /**
